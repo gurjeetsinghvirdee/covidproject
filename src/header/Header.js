5d6cb1982d9c1b17bcec1c7bca2be22c7855a2ea
Nidhi pal <nidhipalmms@gmail.com>
2021-11-28 10:37:59 +0530

    pushmenu position changed

--- src/header/Header.js
+++ src/header/Header.js
@@ -9,9 +9,6 @@ const Header = () => {
             <nav class="main-header navbar navbar-expand navbar-dark">
                 {/* Left navbar links */}
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                    </li>
                     <li class="nav-item d-none d-sm-inline-block">
                         <Link to="/covidproject/"><a href="#" class="nav-link">Home</a></Link>
                     </li>
@@ -20,12 +17,13 @@ const Header = () => {
                 {/* Right navbar links */}
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
+                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                             <i class="fas fa-expand-arrows-alt"></i>
                         </a>
                     </li>
-
-
                 </ul>
             </nav>
             {/* /.navbar */}
